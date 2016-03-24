b9c3aa4dfb891ec04ae318b3edbaa87d59652ff8
saroz khadayat <saroz khadayat>
2016-03-24 21:52:37 +0545

    Training files

---
+++ TrainingDay1/TrainingDay1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using customerModel;
+
+namespace TrainingDay1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Customer customer1 = new Customer();
+        //    Console.WriteLine("Enter First Name:");
+        //   // Console.WriteLine("Enter customer Name:");
+        //      string name=  Console.ReadLine();
+        //      customer1.FirstName = name;
+        //      Console.WriteLine("Enter last Name :");
+        //      customer1.LastName = Console.ReadLine();
+        //   string fullName= customer1.getFullName(customer1.FirstName, customer1.LastName);
+        //      Console.WriteLine("The name is: " +fullName);
+        //      Console.ReadKey();
+            CustomerFromLibrary test = new CustomerFromLibrary{
+            CustomerId=1,
+            FirstName="hey"
+            };
+             
+        }
+    }
+}
---
+++ TrainingDay1/customerModel/CustomerFromLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customerModel
+{
+    public class CustomerFromLibrary
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        public string getFullName(string fname, string lname)
+        {
+            string fullname = fname + " " + lname;
+            return fullname;
+        }
+    }
+}
